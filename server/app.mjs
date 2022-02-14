33a229f8a1d77ab00ff928c9159ad47e5266117f
AlexandrKondakov <sergtankian@mail.ru>
2022-02-14 07:32:46 +0300

    add CORS middleware

--- server/app.mjs
+++ server/app.mjs
@@ -14,6 +14,11 @@ let uptime = 0
 
 const wss = new WebSocketServer({ port: SERVER_PORT + 1 })
 
+server.use((_, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 server.use(express.static('public'))
 
 server.listen(SERVER_PORT, () => {
